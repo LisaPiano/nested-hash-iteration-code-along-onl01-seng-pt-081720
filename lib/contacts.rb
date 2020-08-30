c03ec3a1397ee9b08ec9bb6cd524ce8ee42576fa
Lisa Walters <campi_l@fortlewis.edu>
2020-08-30 02:17:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,11 +19,11 @@ contacts.each do
   |person, contacts_details_hash|
   if person ==
     "Freddy Mercury"
-    contact_details_hash.each do 
+    contact_details_hash.each do
       |attribute, data|
       if attribute == :favorite_ice_cream_flavors
   binding.pry
 end
 end
-end 
+end
 end
