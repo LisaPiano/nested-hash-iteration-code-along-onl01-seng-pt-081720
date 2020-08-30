73fa555f112a03b795d130f527ba3cfa19f4b2ea
Lisa Walters <campi_l@fortlewis.edu>
2020-08-30 02:17:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,13 @@ require 'pry'
 def remove_strawberry(contacts)
 contacts.each do
   |person, contacts_details_hash|
+  if person ==
+    "Freddy Mercury"
+    contact_details_hash.each do 
+      |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
   binding.pry
 end
+end
+end 
 end
